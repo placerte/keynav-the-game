df70933d2323f3f1514b78be4219717a60326314
placerte <pierre.langlois.lacerte@gmail.com>
2025-06-02 00:06:19 -0400

    commencé le minimal gameplay

--- src/keynav-game/my_kivies.py
+++ src/keynav-game/my_kivies.py
@@ -1,18 +1,22 @@
-from kivy.uix.button import Button as kivy_Button
+from kivy.uix.button import Button as KiviBytton
 from typing import Callable, Any
 
-class Button(kivy_Button):
+class Button(KiviBytton):
+
     def bind(
         self,
         *,
-        text: Callable[[kivy_Button, str], None] = None,
-        on_press: Callable[[kivy_Button], None] = None,
+        text: Callable[[KiviBytton, str], None] = None,
+        on_press: Callable[[KiviBytton], None] = None,
+        size: Callable[[KiviBytton], None] = None,
         **kwargs: Callable[..., Any]
     ) -> "Button":
         if text:
             super().bind(text=text)
         if on_press:
             super().bind(on_press=on_press)
+        if size:
+            super().bind(size=size)
         if kwargs:
             super().bind(**kwargs)
         return self
--- src/keynav-game/test.py
+++ src/keynav-game/test.py
@@ -1,21 +1,47 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-#from kivy.uix.button import Button
 from my_kivies import Button
-
+from kivy.uix.floatlayout import FloatLayout
+import numpy as np
 
 class MyApp(App):
 
     button: Button
+    layout: FloatLayout
+    button_rel_pos: tuple[float, float] = (0.5,0.5)
 
     def build(self):
-        self.layout : BoxLayout = BoxLayout(orientation='vertical', padding=50, spacing=20)
-        self.button = Button(text="Click")
-        self.button.bind(on_press=self.show_hello_world)
-        self.layout.add_widget(self.button)
+        self.layout= FloatLayout()
+        self.build_button()
         return self.layout
+
+    def build_button(self):
+        self.button = Button(text="Click")
+        self.button.size_hint = (None, None)
+        self.button.size = (220, 160)
+        self.place_button()
+        self.button.bind(on_press=self.move_button)
+        self.layout.add_widget(self.button)
     
+    def move_button(self, instance):
+        self.choose_next_position()
+        self.place_button()
+
+    def choose_next_position(self):
+        x: float = np.random.random() 
+        y: float = np.random.random() 
+
+        self.button_rel_pos = (x,y)
+
+    def place_button(self):
+        x_rel, y_rel = self.button_rel_pos
+        x: int = int(Window.width* x_rel - (self.button.width/2))
+        y: int = int(Window.height* y_rel - (self.button.height/2))
+
+        self.button.pos = (x,y)
+
     def show_hello_world(self, instance):
         self.label: Label = Label(text="Hello World!")
         self.layout.add_widget(self.label)
